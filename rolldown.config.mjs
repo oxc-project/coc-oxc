284a1b11615f51567edb64cb96fe0e2b4d730e42
Boshen <boshenc@gmail.com>
2025-11-30 20:48:26 +0800

    rolldown don't attachDebugInfo

--- rolldown.config.mjs
+++ rolldown.config.mjs
@@ -12,6 +12,6 @@ export default defineConfig({
   platform: "node",
   minify: process.env.NODE_ENV === "production",
   experimental: {
-    attachDebugInfo: 'none'
-  }
+    attachDebugInfo: "none",
+  },
 });
