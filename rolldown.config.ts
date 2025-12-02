44de2bdd749c865fbd19c9e51482474c9e92eb95
Boshen <boshenc@gmail.com>
2025-12-02 18:11:11 +0800

    rolldown.config.ts

--- rolldown.config.ts
+++ rolldown.config.ts
@@ -5,12 +5,10 @@ export default defineConfig({
   output: {
     file: "lib/index.js",
     format: "cjs",
-    sourcemap: process.env.NODE_ENV === "development",
-    comments: "none",
+    sourcemap: false,
   },
   external: ["coc.nvim"],
   platform: "node",
-  minify: process.env.NODE_ENV === "production",
   experimental: {
     attachDebugInfo: "none",
   },
